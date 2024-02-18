196d8c166c3bec31487f74af0fdd565a8ca7c0c6
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-02-18 20:34:36 -0300

    add qa2 properties file

--- src/main/SQL/mysql-service-account.sql
+++ src/main/SQL/mysql-service-account.sql
@@ -4,3 +4,10 @@ GRANT SELECT ON springguru.* to 'springframework'@'%';
 GRANT INSERT ON springguru.* to 'springframework'@'%';
 GRANT DELETE ON springguru.* to 'springframework'@'%';
 GRANT UPDATE ON springguru.* to 'springframework'@'%';
+
+CREATE USER IF NOT EXISTS 'qa2user'@'%' IDENTIFIED WITH mysql_native_password BY 'qa2password';
+
+GRANT SELECT ON qa2.* TO 'qa2user'@'%';
+GRANT INSERT ON qa2.* TO 'qa2user'@'%';
+GRANT DELETE ON qa2.* TO 'qa2user'@'%';
+GRANT UPDATE ON qa2.* TO 'qa2user'@'%';
